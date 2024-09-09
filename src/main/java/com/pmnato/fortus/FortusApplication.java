50118bc2957fc885b096b5a41ed8c0c4a1dda619
Deb4cker <ndebacher@gmail.com>
2024-09-09 19:44:56 -0300

    Load project

---
+++ src/main/java/com/pmnato/fortus/FortusApplication.java
@@ -0,0 +1,13 @@
+package com.pmnato.fortus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmnato/fortus/FortusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmnato.fortus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
